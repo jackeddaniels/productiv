fc717856857cdf6a1a94a3b63c733df4c695be56
John Flores <johnflores1991@hotmail.com>
2023-11-03 13:00:03 -0500

    added footer testing, removed from app test file

--- src/App.test.js
+++ src/App.test.js
@@ -18,12 +18,6 @@ describe("productiv app", function () {
     expect(result.queryByText("Prøductïv")).toBeInTheDocument();
   });
 
-  it("contains expected Footer", function () {
-    const { container } = render(<Footer />);
-    const footer = container.querySelector('footer');
-    expect(container).toContainHTML('Prødutïv is copyright ©2020')
-  });
-
   // it ("rendered quotes app", function () {
   //   const result = render(<App />);
   //   expect(result.queryByText("Click here for an inspirational quøte!")).toBeInTheDocument();
--- src/Footer.test.js
+++ src/Footer.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Footer from "./Footer";
 
-describe("productiv app", function () {
+describe("Footer renders", function () {
   it("renders without crashing", function () {
     render(<Footer />);
   });
@@ -10,6 +10,12 @@ describe("productiv app", function () {
   it("matches snapshot", function () {
     const { container } = render(<Footer />);
     expect(container).toMatchSnapshot();
+  });
+
+  it("has correct footer data", function () {
+    const { container } = render(<Footer />);
+    const footer = container.querySelector('footer');
+    expect(container).toContainHTML('Prødutïv is copyright ©2020')
   });
 });
 
